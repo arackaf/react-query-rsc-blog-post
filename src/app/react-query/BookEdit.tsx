9e766f099e40120c80ccf0577f82cc2b93fcfa6b
Adam Rackis <adamrackis@hotmail.com>
2024-05-21 12:18:18 -0500

    ui tweaks

--- src/app/react-query/BookEdit.tsx
+++ src/app/react-query/BookEdit.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC, useRef } from "react";
+import { FC, useRef, useTransition } from "react";
 import { BookEditProps } from "../types";
 import { useQueryClient } from "@tanstack/react-query";
 
@@ -8,23 +8,28 @@ export const BookEdit: FC<BookEditProps> = (props) => {
   const { book } = props;
   const titleRef = useRef<HTMLInputElement>(null);
   const queryClient = useQueryClient();
+  const [saving, startSaving] = useTransition();
 
   const saveBook = async (id: number, newTitle: string) => {
-    await fetch("/api/books/update", {
-      method: "POST",
-      body: JSON.stringify({
-        id,
-        title: newTitle,
-      }),
-    });
+    startSaving(async () => {
+      await fetch("/api/books/update", {
+        method: "POST",
+        body: JSON.stringify({
+          id,
+          title: newTitle,
+        }),
+      });
 
-    queryClient.invalidateQueries({ queryKey: ["books-query"] });
+      await queryClient.invalidateQueries({ queryKey: ["books-query"] });
+    });
   };
 
   return (
     <div className="flex gap-2">
       <input className="border rounded border-gray-600 p-1" ref={titleRef} defaultValue={book.title} />
-      <button onClick={() => saveBook(book.id, titleRef.current!.value)}>Save</button>
+      <button className="rounded border border-gray-600 p-1 bg-blue-300" disabled={saving} onClick={() => saveBook(book.id, titleRef.current!.value)}>
+        {saving ? "Saving..." : "Save"}
+      </button>
     </div>
   );
 };
--- src/app/rsc/BookEdit.tsx
+++ src/app/rsc/BookEdit.tsx
@@ -1,17 +1,26 @@
 "use client";
 
-import { FC, useRef } from "react";
+import { FC, useRef, useTransition } from "react";
 import { saveBook } from "../serverActions";
 import { BookEditProps } from "../types";
 
 export const BookEdit: FC<BookEditProps> = (props) => {
   const { book } = props;
   const titleRef = useRef<HTMLInputElement>(null);
+  const [saving, startSaving] = useTransition();
+
+  function doSave() {
+    startSaving(async () => {
+      await saveBook(book.id, titleRef.current!.value);
+    });
+  }
 
   return (
     <div className="flex gap-2">
       <input className="border rounded border-gray-600 p-1" ref={titleRef} defaultValue={book.title} />
-      <button onClick={() => saveBook(book.id, titleRef.current!.value)}>Save</button>
+      <button className="rounded border border-gray-600 p-1 bg-blue-300" disabled={saving} onClick={doSave}>
+        {saving ? "Saving..." : "Save"}
+      </button>
     </div>
   );
 };
